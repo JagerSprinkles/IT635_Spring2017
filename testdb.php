88d94d421855fcbd66d2a5bacc9c865496707f3a
Nick Carbonara <Jager.Sprinkles@gmail.com>
2017-02-01 20:38:54 -0500

    End of class 2-1-17

--- testdb.php
+++ testdb.php
@@ -6,6 +6,16 @@ require_once("studentDB.inc");
 
 echo "executing script: ".$argv[0].PHP_EOL;
 
+$studentName = $argv[1];
+$studentID = $argv[2];
+$studentAddress = $argv[3];
+$studentYear = argv[4];
+
+$studentDB = new StudentAccess("classes");
+
+//$studentDB->
+
+
 
 $studentDB = new StudentAccess("classes");
 
