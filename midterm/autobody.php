fe5c72763e56ff56a99b0ca253eaf9eebf9a925a
Nick Carbonara <Jager.Sprinkles@gmail.com>
2017-03-17 19:52:46 -0400

    Bug fixes and performance improvements

--- midterm/autobody.php
+++ midterm/autobody.php
@@ -2,7 +2,7 @@
 
 <?php
 require_once("bodyShopDB.inc");
-
+system('clear');
 echo "Welcome to the Übertastic Auto Body Car and Part Tracker Thingy, or ÜABCPTT! \nPress CTRL+c to exit at any time.\n\n";
 
 $BSDB = new BodyShopAccess();
@@ -24,11 +24,12 @@ while ("Tuesday") {
 				}
 			} else {
 				$menu = 1;
-				system('clear');
+
 			}
 			break;
 			
 			case '1': //main menu
+			system('clear');
 			echo "-----Main Menu-----\nPlease type the number for the option you would like to choose:\n1. Add vehicle to database.\n2. Check or update status of a vehicle.\n3. Check stock levels on all parts.\n4. Order a part.\n5. Show the most ordered parts.\n\n";
 			$next = trim(fgets(STDIN));
 			switch ($next) {
@@ -50,75 +51,81 @@ while ("Tuesday") {
 				break;
 
 				case '2': //Check or update status of a vehicle
+				system('clear');
 				echo "Please enter the customer's last name to select their vehicle\n";
 				$lname = trim(fgets(STDIN));// get cur stat of vehicle here
 				//$BSDB = new BodyShopAccess();
 				$result = $BSDB->getVehicleStatus($lname);
+				system('clear');
 				if ($result['fname']){
-					echo "\n".$result['fname']." ".$result['lname']."'s vehicle's status is currently '".$result['status']."'.\nIf you would like to update the status please type 'update', otherwise press enter to return to the main menu\n\n"; 
+					echo $result['fname']." ".$result['lname']."'s vehicle's status is currently '".$result['status']."'.\nIf you would like to update the status please type 'update', otherwise press enter to return to the main menu\n\n"; 
 					$next = trim(fgets(STDIN));
 					if ($next == "update"){
+						system('clear');
 						echo "Please enter the new status of the vehicle:\n1. Needs estimate.\n2. Waiting for authorization to begin repairs.\n3. Waiting for parts.\n4. Repairs underway!\n5. In paint shop.\n6. In detail shop.\n7. Ready for customer pickup.\n\n";
 						$next = trim(fgets(STDIN));
+						system('clear');
 						switch ($next) {
 							case '1':
 							if ($BSDB->setVehicleStatus($lname,'Needs estimate')){
 								$result = $BSDB->getVehicleStatus($lname);
-								echo "\n".$result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
+								echo $result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
 
-							} else echo "There was an error somewhere, you shouldn't see this...";
+							} else echo "There was an error somewhere, you shouldn't see this...\n";
 
 							break;
 							case '2':
 							if ($BSDB->setVehicleStatus($lname,'Waiting for authorization to begin repairs')){
 								$result = $BSDB->getVehicleStatus($lname);
-								echo "\n".$result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
+								echo $result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
 
-							} else echo "There was an error somewhere, you shouldn't see this...";
+							} else echo "There was an error somewhere, you shouldn't see this...\n";
 
 							break;
 							case '3':
 							if ($BSDB->setVehicleStatus($lname,'Waiting for parts')){
 								$result = $BSDB->getVehicleStatus($lname);
-								echo "\n".$result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
+								echo $result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
 
-							} else echo "There was an error somewhere, you shouldn't see this...";
+							} else echo "There was an error somewhere, you shouldn't see this...\n";
 
 							break;
 							case '4':
 							if ($BSDB->setVehicleStatus($lname,'Repairs underway!')){
 								$result = $BSDB->getVehicleStatus($lname);
-								echo "\n".$result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
+								echo $result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
 
-							} else echo "There was an error somewhere, you shouldn't see this...";
+							} else echo "There was an error somewhere, you shouldn't see this...\n";
 
 							break;
 							case '5':
 							if ($BSDB->setVehicleStatus($lname,'In paint shop')){
 								$result = $BSDB->getVehicleStatus($lname);
-								echo "\n".$result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
+								echo $result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
 
-							} else echo "There was an error somewhere, you shouldn't see this...";
+							} else echo "There was an error somewhere, you shouldn't see this...\n";
 
 							break;
 							case '6':
 							if ($BSDB->setVehicleStatus($lname,'In detail shop')){
 								$result = $BSDB->getVehicleStatus($lname);
-								echo "\n".$result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
+								echo $result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
 
-							} else echo "There was an error somewhere, you shouldn't see this...";
+							} else echo "There was an error somewhere, you shouldn't see this...\n";
 
 							break;
 							case '7':
 							if ($BSDB->setVehicleStatus($lname,'Ready for customer pickup')){
 								$result = $BSDB->getVehicleStatus($lname);
-								echo "\n".$result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
+								echo $result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
 
-							} else echo "There was an error somewhere, you shouldn't see this...";
+							} else echo "There was an error somewhere, you shouldn't see this...\n";
 
 							break;
 
 						}
+						echo "Please press enter to return to the main menu.\n";
+						trim(fgets(STDIN));
 					}else break;
 				}
 				else echo "Customer '".$lname."' not found, returning to the main menu.\n\n";
@@ -126,8 +133,8 @@ while ("Tuesday") {
 				break;
 
 				case '3': //Check stock levels on all parts
-
-				echo $BSDB->getPartStock()."\nPlease press enter to return to the main menu\n";
+				system('clear');
+				echo $BSDB->getPartStock()."\nPlease press enter to return to the main menu.\n";
 				trim(fgets(STDIN));
 				break;
 
