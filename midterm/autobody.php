7f026bcccc5e0a4a8bad4c3502934b55f9faaef2
Nick Carbonara <Jager.Sprinkles@gmail.com>
2017-03-18 13:31:06 -0400

    Bug fixes and performance improvements

--- midterm/autobody.php
+++ midterm/autobody.php
@@ -61,10 +61,20 @@ while ("Tuesday") {
 		
 		case '1': //main menu
 		system('clear');
-		echo "-----Main Menu-----\nPlease type the number for the option you would like to choose:\n1. Get current vehicle list.\n2. Add vehicle to database.\n3. Check or update status of a vehicle.\n4. Check stock levels on all parts.\n5. Order a part.\n6. Show the most ordered parts.\n7. Exit.\n\n";
+		echo str_pad("Main Menu", exec('tput cols'), "=", STR_PAD_BOTH);
+		echo "\nPlease type the number for the option you would like to choose:\n1. Get current vehicle list.\n2. Add vehicle to database.\n3. Check or update status of a vehicle.\n4. Check stock levels on all parts.\n5. Order a part.\n6. Show the most ordered parts.\n7. Exit.\n\n";
 		$next = trim(fgets(STDIN));
 		switch ($next) {
-			case '1': //Add vehicle to database
+			case '1':
+			system('clear');
+			echo $BSDB->getVehicles();
+			echo "\nPlease press enter to return to the main menu.\n";
+			trim(fgets(STDIN));
+			break;
+
+
+			case '2': //Add vehicle to database
+			system('clear');
 			echo "Please enter the customer's first name.\n";
 			$fname = trim(fgets(STDIN));
 			echo "Please enter the customer's last name.\n";
@@ -84,11 +94,12 @@ while ("Tuesday") {
 			echo "Please enter the vehicle's color.\n";
 			$color = trim(fgets(STDIN));
 			$BSDB->addVehicle($fname,$lname,$make,$model,$year,$color);
-			echo $fname." ".$lname."'s vehicle has been added to the database.\n"
+			echo "\n".$fname." ".$lname."'s vehicle has been added to the database.\n\n";
+			echo "Please press enter to return to the main menu.\n";
 			trim(fgets(STDIN));
 			break;
 
-			case '2': //Check or update status of a vehicle
+			case '3': //Check or update status of a vehicle
 			system('clear');
 			echo "Please enter the customer's last name to select their vehicle\n";
 			$lname = trim(fgets(STDIN));// get cur stat of vehicle here
@@ -127,7 +138,7 @@ while ("Tuesday") {
 						break;
 					}
 					$result = $BSDB->getVehicleStatus($lname);
-					echo $result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n";
+					echo $result['fname']." ".$result['lname']."'s vehicle's status has been updated to '".$result['status']."'.\n\n";
 					echo "Please press enter to return to the main menu.\n";
 					
 				}else break;
@@ -136,14 +147,15 @@ while ("Tuesday") {
 			trim(fgets(STDIN));
 			break;
 
-			case '3': //Check stock levels on all parts
+			case '4': //Check stock levels on all parts
 			system('clear');
 			echo $BSDB->getPartStock(exec('tput cols'))."\nPlease press enter to return to the main menu.\n";
 			trim(fgets(STDIN));
 			break;
 
 
-			case '4': //Order a part
+			case '5': //Order a part
+			system('clear');
 			echo "Please choose the part you would like to order.\nInstant delivery is guaranteed!\n\n1. Front bumper.\n2. Rear bumper.\n3. Fender.\n4. Quarter panel.\n5. Hood.\n6. Front door.\n7. Rear door.\n8. Headlight.\n9. Taillight.\n\n";
 			$next = trim(fgets(STDIN));
 			$part = "";
@@ -178,22 +190,33 @@ while ("Tuesday") {
 			}
 			echo "How many ".$part."s would you like to order?\n";
 			$amount = trim(fgets(STDIN));
+			if (!is_numeric($amount)) $amount = 'THE GAME';
+			while (!is_numeric($amount)) {
+				echo "That was not a number, please try again.\n";
+				$amount = trim(fgets(STDIN));
+				if (!is_numeric($amount)) $amount = 'THE GAME';
+			}
 			$stock = $BSDB->orderPart($part,$amount);
-			echo "The new stock level of ".$part."s is ".$stock.".\n";
+			system('clear');
+			echo "The new stock level of ".$part."s is ".$stock.".\n\n";
+			echo "Please press enter to return to the main menu.\n";
 			trim(fgets(STDIN));
 			
 			break;
-			case '5': //Show the most ordered parts
+			case '6': //Show the most ordered parts
+			system('clear');
 			echo $BSDB->getMostOrdered(exec('tput cols'));
+			echo "\nPlease press enter to return to the main menu.\n";
 			trim(fgets(STDIN));
 			break;
-			case '6':
-			goto OMGWHYONEARTHWOULDYOUUSEAGOTO;
+			case '7':
+			goto VELOCIRAPTOR_ATTACK; //https://xkcd.com/292/
 
 		}
 		break;
 
 		case '69':
+		system('clear');
 		echo "You can type SQL statements here and they will be sent to the database. Any output will be displayed here after the command runs.";
 		$next = trim(fgets(STDIN));
 
@@ -203,7 +226,7 @@ while ("Tuesday") {
 
 	//echo "end loop\n" . $next . "\n";
 }
-OMGWHYONEARTHWOULDYOUUSEAGOTO:
+VELOCIRAPTOR_ATTACK:
 system('clear');
 exit();
 ?>
